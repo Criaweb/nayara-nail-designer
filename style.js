2fc677ddbb706395bf8d53781399dbd48113fbf3
Ariele Silva <139187649+arielesilvaa@users.noreply.github.com>
2024-10-03 11:03:13 -0300

    ok

--- style.js
+++
@@ -1,4 +0,0 @@
-Alongamento de Fibra
-
-
-Técnica para alongamento e reforço das unhas, proporcionando uma estrutura leve e durável. Aplicada sobre a unha natural, oferecendo um acabamento resistente e de aparência natural.
